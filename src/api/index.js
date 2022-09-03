041a91d0c10ae7f349ec5beefcf8d61847e46819
mahendramahato <74789335+mahendramahato@users.noreply.github.com>
2022-09-03 07:20:52 -0500

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -8,7 +8,7 @@ export const getAllSongs = async (queryword) => {
         const response = await axios.get(URL, {
           params: {query: queryword},
           headers: {
-            'X-RapidAPI-Key': 'f19236b2dbmshd94c17dab305d9ep1bebccjsn3aae7d283d5e',
+            'X-RapidAPI-Key': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
             'X-RapidAPI-Host': 'youtube-music1.p.rapidapi.com'
           }
         })
